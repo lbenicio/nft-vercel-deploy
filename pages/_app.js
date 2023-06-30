a90fd9f11128205946ee98f5355a44c6debfde08
Leonardo Benicio <5027245+lbenicio@users.noreply.github.com>
2023-06-30 11:55:48 -0300

    init

---
+++ pages/_app.js
@@ -0,0 +1,36 @@
+import { useMemo } from "react";
+import { clusterApiUrl } from "@solana/web3.js";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets"
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react"
+
+import "../styles/App.css"
+import "../styles/globals.css"
+import "../styles/CandyMachine.css"
+import "@solana/wallet-adapter-react-ui/styles.css"
+
+const App = ({ Component, pageProps }) => {
+  const network = WalletAdapterNetwork.Devnet
+  const endpoint = useMemo(() => clusterApiUrl(network), [network])
+  const wallets = useMemo(
+    () => [new PhantomWalletAdapter(), new SolflareWalletAdapter(), new TorusWalletAdapter()],
+    [network]
+  )
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
+}
+
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,52 @@
+import React from "react"
+import dynamic from "next/dynamic"
+import { useWallet } from "@solana/wallet-adapter-react";
+import CandyMachine from "../components/CandyMachine";
+
+// Constants
+const TWITTER_HANDLE = "lbenicio_"
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
+
+const Home = () => {
+  const WalletMultiButtonDynamic = dynamic(
+    async () => (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+    { ssr: false }
+  )
+
+  const wallet = useWallet()
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+
+      <div className="button-container">
+        <WalletMultiButtonDynamic className="cta-button connect-wallet-button" />
+      </div>
+    </div>
+  )
+
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header">🍭 Pokemon Candy Machine</p>
+          <img src='0.png'></img>
+          <p className="sub-text">Candy Machine mint NFTs</p>
+          {wallet.publicKey ? <CandyMachine walletAddress={wallet} /> : renderNotConnectedContainer()}
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`construído pela @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
